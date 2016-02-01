ff2b743b41ddc1bd286539c6c12750a88ca2fb02
OmondiKevin <kevochi09@gmail.com>
2016-02-01 16:47:27 +0300

    Updated procedures

--- dbv/data/schema/proc_update_sync_regimen_category.sql
+++ dbv/data/schema/proc_update_sync_regimen_category.sql
@@ -3,10 +3,9 @@
 
 -- To call- call update_sync_regimen_category(17,'Adult Third Line','1',2);
 
-DELIMITER $$
 
-DROP PROCEDURE IF EXISTS `update_sync_regimen_category` $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `update_sync_regimen_category`(
+DROP PROCEDURE IF EXISTS `proc_update_sync_regimen_category`;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `proc_update_sync_regimen_category`(
   IN loc_id INT(11),
   IN loc_Name VARCHAR(50),
   IN loc_Active VARCHAR(2),
@@ -14,10 +13,8 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `update_sync_regimen_category`(
 )
 BEGIN
 
-  INSERT INTO `update_sync_regimen_category` (id, Name, Active, ccc_store_sp)
+  INSERT INTO `sync_regimen_category` (id, Name, Active, ccc_store_sp)
   	VALUES(loc_id, loc_Name, loc_Active, loc_ccc_store_sp);
 
 
-END $$
-
-DELIMITER ;
+END
