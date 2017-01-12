a639b4467101138fb0292ad58bfaf20d85cf3364
victoriawasonga <victoriawasonga@gmail.com>
2017-01-12 16:57:51 +0300

    new changes

--- application/views/sections/link.php
+++ application/views/sections/link.php
@@ -1,2 +1,2 @@
-<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14745207861cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
-<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1474520786e3269901e7f60dbcfcacfa964593fb6d.js" charset="UTF-8"></script>
+<link type="text/css" rel="stylesheet" href="http://localhost:80/ADT/assets/cache/14839292801cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
+<script type="text/javascript" src="http://localhost:80/ADT/assets/cache/1483929280e3269901e7f60dbcfcacfa964593fb6d.js" charset="UTF-8"></script>
