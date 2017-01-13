a2719bb9de2779df13409d71dd99b627f21ab5b5
OmondiKevin <kecovhi09@gmail.com>
2017-01-13 17:40:46 +0300

    #Fix: Ensure icons are visible when port is changed from 80
    #Fix: Remove the use of cache and compression of .css and .js files

---
+++ application/views/sections/link.php
@@ -0,0 +1,2 @@
+<link type="text/css" rel="stylesheet" href="http://localhost:81/ADT/assets/cache/14762737741cd61b25796ccb6cbf22d8e1527cc816.css" media="screen" />
+<script type="text/javascript" src="http://localhost:81/ADT/assets/cache/1476273774e3269901e7f60dbcfcacfa964593fb6d.js" charset="UTF-8"></script>
